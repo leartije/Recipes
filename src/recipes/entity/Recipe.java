dabed9ebc8a25025017a025c8baf858aae770f87
leartije <reciska@gmail.com>
2022-06-18 14:57:55 +0200

    stage 2 complete

--- src/recipes/entity/Recipe.java
+++ src/recipes/entity/Recipe.java
@@ -1,17 +1,22 @@
 package recipes.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Recipe {
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Long id;
     private String name;
     private String description;
-    private String ingredients;
-    private String directions;
+    private String[] ingredients;
+    private String[] directions;
 
 }
