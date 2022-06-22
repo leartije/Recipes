52f346b03d2a8094ac16b4093896a87d8774d0b7
leartije <reciska@gmail.com>
2022-06-22 11:01:42 +0200

    stage 3 complete

--- src/recipes/services/RecipeServices.java
+++ src/recipes/services/RecipeServices.java
@@ -2,13 +2,18 @@ package recipes.services;
 
 import recipes.entity.Recipe;
 
+import java.util.List;
+
 public interface RecipeServices {
 
+    List<Recipe> printAllRecipes();
 
-    public Recipe printAllRecipe();
-
-    public Recipe getRecipeById(Long id);
+    Recipe getRecipeById(Long id);
 
     Recipe saveRecipe(Recipe recipe);
+
+    void deleteRecipeById(Long id);
+
+
 
 }
